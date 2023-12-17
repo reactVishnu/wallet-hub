42b197bf86626bc42340fb080fad5cfd76be500f
reactVishnu <thirdparties47@gmail.com>
2023-12-17 23:00:15 +0530

    Featuer #4

--- wallet_hub/core/admin.py
+++ wallet_hub/core/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Detail
+from django.contrib.auth import get_user_model
 # Register your models here.
 
 admin.site.register(Detail)
+admin.site.register(get_user_model())
